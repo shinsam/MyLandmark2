e5dc56b3e0805ba5dac31f6b203ab8458858323f
shinsam <shin321gg@gmail.com>
2022-05-16 14:12:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mylandmark/InfoActivity.java
@@ -0,0 +1,26 @@
+package com.example.mylandmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class InfoActivity extends AppCompatActivity {
+
+    ImageView   img_info;
+    TextView    txt_info;
+    int imgno = 0;
+    int imgs[] = {R.drawable.info1 , R.drawable.info2,
+            R.drawable.info3,R.drawable.info4,
+            R.drawable.info5   };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        img_info = findViewById(R.id.img_info);
+        txt_info = findViewById(R.id.txt_info);
+    }
+}
